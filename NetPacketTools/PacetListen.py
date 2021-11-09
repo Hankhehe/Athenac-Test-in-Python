c3c2cfc48d7215450216a5dd5c3fbea86fb5ce0c
Hank Chen <hank@pixis.com.tw>
2021-11-09 22:39:34 +0800

    ListenClassnotyet

---
+++ NetPacketTools/PacetListen.py
@@ -0,0 +1,19 @@
+from scapy.all import sniff
+from collections import Counter
+
+
+## Create a Packet Counter
+packet_counts = Counter()
+
+## Define our Custom Action function
+def custom_action(packet):
+    # Create tuple of Src/Dst in sorted order
+    key = tuple(sorted([packet[0][1].src, packet[0][1].dst]))
+    packet_counts.update([key])
+    return f"Packet #{sum(packet_counts.values())}: {packet[0][1].src} ==> {packet[0][1].dst}"
+
+## Setup sniff, filtering for IP traffic
+sniff(filter="ip", prn=custom_action, count=1000,iface='Wi-Fi')
+
+## Print out packet count per A <--> Z address pair
+print("\n".join(f"{f'{key[0]} <--> {key[1]}'}: {count}" for key, count in packet_counts.items()))
