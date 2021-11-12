916a436ee5baa9644508b43cc2e270d3d5757c3a
Hank Chen <hank@pixis.com.tw>
2021-11-12 23:38:48 +0800

    fix packetlisten

--- NetPacketTools/PacketListen.py
+++ NetPacketTools/PacketListen.py
@@ -13,26 +13,27 @@ class PacketListen:
         self.linklocalIP = [x for x in self.nic.ips[6] if 'fe80::' in x]
         self.globallIP = [x for x in self.nic.ips[6] if '2001:' in x]
         self.checklist ={'WinodwsOS':False,'Linux':False,'iOS':False,'Clock':False,'Printer':False}
-# 'ether src {ProbeMAC} and ((udp and src port 45231 and dst port 45231) or (tcp and src port 18005 and dst port 445)\
-#             or (tcp and src port 18006 and dst port 62078) or (tcp and src port 18009 and (dst port 1621 or dst port 515 or dst port 9100 or dst port 631)) or arp)'
-        #if filte then need setting that, exsample : fillter = 'arp or udp or tcp' for sniff() etc.....
-        sniff(filter =f'ether src {self.probeMAC} and ((udp and src port 45231 and dst port 45231) or (tcp and src port 18005 and dst port 445)\
-             or (tcp and src port 18006 and dst port 62078) or (tcp and src port 18009 and (dst port 1621 or dst port 515 or dst port 9100 or dst port 631)))'
-        ,store = 0,prn=self.CheckPacket ,timeout =1900 ,iface=self.nicName)
 
-    def CheckPacket(self,Packet):
-        
-        if '45231' in Packet: self.checklist['Linux'] = True;return
-        elif '18005' and '445'in Packet: self.checklist['WinodwsOS'] = True;return
-        elif '18006' and '62078' in Packet : self.checklist['iOS']=True;return
-        elif '18009' and ('4660' or '1621') in Packet : self.checklist['Clock'] = True;return
-        elif '18009' and ('515' or '9100' or '631') in Packet: self.checklist['Printer'] = True;return
-        
+        #if filte then need setting that, exsample : filter = 'arp or udp or tcp' for sniff() etc.....
+        sniff(filter=f'ether src {ProbeMAC} and ether dst {self.mac} and ((udp and src port 45231 and dst port 45231) or (tcp and src port 18005 and dst port 445)\
+             or (tcp and src port 18006 and dst port 62078) or (tcp and src port 18009 and (dst port 4660 or dst port 1621 or dst port 515 or dst port 9100 or dst port 631)))'
+        ,store = 0,prn=self.CheckTCPorUDP ,timeout =1900 ,iface=self.nicName)
         print(self.checklist)
+
+    def CheckTCPorUDP(self,Packet):
+        if TCP in Packet:  self.CheckTCPPacket(Packet)
+        elif UDP in Packet: self.CheckUDPPacket(Packet)
+
+    def CheckTCPPacket(self,Packet):
+        if Packet['TCP'].sport ==  18005 and Packet['TCP'].dport == 445: self.checklist['WinodwsOS']=True
+        elif Packet['TCP'].sport == 18006 and Packet['TCP'].dport == 62078: self.checklist['iOS']=True
+        elif Packet['TCP'].sport == 18009 \
+            and (Packet['TCP'].dport == 1621 or Packet['TCP'].dport == 515 or Packet['TCP'].dport == 9100 or Packet['TCP'].dport==631):
+            self.checklist['Printer']=True 
+        elif Packet['TCP'].sport == 18009 and Packet['TCP'].dport == 1621 or Packet['TCP'].dport == 4660: self.checklist['Clock']=True
+
+    def CheckUDPPacket(self,Packet):
+        if Packet['UDP'].sport ==  45231 and Packet['UDP'].dport == 45231: self.checklist['Linux']=True
 
     def printPacket(self,Packet):
         print(Packet.summary())
-
-
-pcapfile = rdpcap('D:/SMB.pcap')
-PacketListen('b8:27:eb:63:9b:01')
