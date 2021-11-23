1e756ba36400c925d9034e7dcfebbb085587c637
unknown <user@Win10Hank.pixis.com>
2021-11-23 16:59:17 +0800

    fix

--- NetPacketTools/packet_action_test.py
+++ NetPacketTools/packet_action_test.py
@@ -22,14 +22,15 @@ class PacketActionTest(PacketAction):
         presharkey = b'pixis'
         hexnowtime = hex(int(time.time()))
         serverIP ='192.168.10.233'
-        radiuspacket = Radius(id=6,code=40,authenticator=bytes.fromhex('0'*32)
+        radiuspacket = Radius(id=90,code=40,authenticator=bytes.fromhex('0'*32)
         ,attributes=[RadiusAttr_NAS_IP_Address(value=nasip)
         ,RadiusAttribute(type=31,len=2+len(callmac),value=callmac)
         ,RadiusAttribute(type=49,value=bytes.fromhex('00000006'))
         ,RadiusAttribute(type=55,value=bytes.fromhex(hexnowtime[2::]))
         ,RadiusAttribute(type=80,value=bytearray.fromhex('0'*32))
-        ,RadiusAttr_Vendor_Specific(vendor_id=9,vendor_type=1,value=b'subscriber:command=reauthenticate')
-        ,RadiusAttr_Vendor_Specific(vendor_id=9,vendor_type=1,value=b'audit-session-id=AC11FFE90000025B5B1329D4')
+        # ,RadiusAttr_Vendor_Specific(vendor_id=9,vendor_type=1,value=b'subscriber:command=reauthenticate')
+        # ,RadiusAttr_Vendor_Specific(vendor_id=9,vendor_type=1,value=b'audit-session-id=AC11FFE90000027A5FA080B6')
+        # ,RadiusAttribute(type=44,value=b'AC11FFE90000027A5FA080B6')
         ])
 
         MessageAuth = hmac.new(presharkey,bytes(radiuspacket),hashlib.md5).hexdigest()
