407ad986aec3d527c5a599d03dc30f5ca9c0bafc
unknown <user@Win10Hank.pixis.com>
2021-11-23 12:54:48 +0800

    fix

--- NetPacketTools/packet_action_test.py
+++ NetPacketTools/packet_action_test.py
@@ -18,44 +18,52 @@ class PacketActionTest(PacketAction):
     
     def SendRadiusCoARequest(self):
         nasip =b'192.168.21.180'
-        callmac=b'B8-27-EB-A3-5F-14'
+        callmac=b'B8:27:EB:A3:5F:14'
+        presharkey = b'pixis'
         hexnowtime = hex(int(time.time()))
-        radiuspacket = Radius(id=1,code=40,authenticator=bytes.fromhex('0'*32)
+        serverIP ='192.168.10.233'
+        radiuspacket = Radius(id=6,code=40,authenticator=bytes.fromhex('0'*32)
         ,attributes=[RadiusAttr_NAS_IP_Address(value=nasip)
-        ,RadiusAttribute(type=31,len=19,value=callmac)
+        ,RadiusAttribute(type=31,len=2+len(callmac),value=callmac)
         ,RadiusAttribute(type=49,value=bytes.fromhex('00000006'))
         ,RadiusAttribute(type=55,value=bytes.fromhex(hexnowtime[2::]))
         ,RadiusAttribute(type=80,value=bytearray.fromhex('0'*32))
-        # ,RadiusAttr_Vendor_Specific(vendor_id=9,vendor_type=1,value=b'subscriber:command=reauthenticate')
+        ,RadiusAttr_Vendor_Specific(vendor_id=9,vendor_type=1,value=b'subscriber:command=reauthenticate')
         ,RadiusAttr_Vendor_Specific(vendor_id=9,vendor_type=1,value=b'audit-session-id=AC11FFE90000025B5B1329D4')
         ])
 
-        MessageAuth = hmac.new(b'pixis',bytes(radiuspacket),hashlib.md5).hexdigest()
+        MessageAuth = hmac.new(presharkey,bytes(radiuspacket),hashlib.md5).hexdigest()
         radiuspacket.attributes[4].value = bytes.fromhex(MessageAuth)
 
-        authenticator = hashlib.md5(bytes(radiuspacket)+b'pixis').hexdigest()
+        authenticator = hashlib.md5(bytes(radiuspacket)+presharkey).hexdigest()
         radiuspacket.authenticator = bytes.fromhex(authenticator)
         readiusCoArequestpacket = Ether(src =self.mac,dst=self.GetIPv4MAC('192.168.21.254'))\
-         /IP(src=self.Ip,dst='192.168.10.233')\
+         /IP(src=self.Ip,dst=serverIP)\
             /UDP(sport =51818,dport=3799)\
                /radiuspacket
         wrpcap('C:/Users/Public/CoA.pcap',readiusCoArequestpacket)
         sendp(readiusCoArequestpacket)
 
     def CalculateHashFromCustomerPacket(self): #計算客戶封包的 Hash key
-        packetpcap = rdpcap('C:/Users/Public/CoACustomer.pcap')
-        presharkey = b'cisco'
-        radiuspacket = Radius(packetpcap[0]['Raw'].load)
-        radiuspacket.authenticator = bytes.fromhex('0'*32)
-        print('authenticator : '+hashlib.md5(bytes(radiuspacket)+presharkey).hexdigest())
-        radiuspacket['RadiusAttr_Message_Authenticator'].value = bytes.fromhex('0'*32)
-        print('Message-Authen : ' +hmac.new(presharkey,bytes(radiuspacket),hashlib.md5).hexdigest())
+        try:
+            packetpcap = rdpcap('C:/Users/Public/CoACustomer.pcap')
+            presharkey = b'cisco'
+            radiuspacket = Radius(packetpcap[0]['Raw'].load)
+            radiuspacket.authenticator = bytes.fromhex('0'*32)
+            print('authenticator : '+hashlib.md5(bytes(radiuspacket)+presharkey).hexdigest())
+            radiuspacket['RadiusAttr_Message_Authenticator'].value = bytes.fromhex('0'*32)
+            print('Message-Authen : ' +hmac.new(presharkey,bytes(radiuspacket),hashlib.md5).hexdigest())
+        except:
+            print('Error Packet')
 
     def CalculateHashFromPacket(self): #計算封包的 Hash key
-        packetpcap = rdpcap('C:/Users/Public/CoA.pcap')
-        presharkey = b'pixis'
-        radiuspacket = packetpcap[0]['Radius']
-        radiuspacket.authenticator = bytes.fromhex('0'*32)
-        print('authenticator : '+hashlib.md5(bytes(radiuspacket)+presharkey).hexdigest())
-        radiuspacket['RadiusAttr_Message_Authenticator'].value = bytes.fromhex('0'*32)
-        print('Message-Authen : ' +hmac.new(presharkey,bytes(radiuspacket),hashlib.md5).hexdigest())
+        try:
+            packetpcap = rdpcap('C:/Users/Public/CoA.pcap')
+            presharkey = b'pixis'
+            radiuspacket = packetpcap[0]['Radius']
+            radiuspacket.authenticator = bytes.fromhex('0'*32)
+            print('authenticator : '+hashlib.md5(bytes(radiuspacket)+presharkey).hexdigest())
+            radiuspacket['RadiusAttr_Message_Authenticator'].value = bytes.fromhex('0'*32)
+            print('Message-Authen : ' +hmac.new(presharkey,bytes(radiuspacket),hashlib.md5).hexdigest())
+        except:
+            print('Error Packet')
