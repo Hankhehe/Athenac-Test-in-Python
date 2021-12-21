7048386f78364d424170c2f8460fff964210aeff
unknown <user@Win10Hank.pixis.com>
2021-12-21 12:32:34 +0800

    fix

--- test_expect.py
+++ test_expect.py
@@ -2,9 +2,9 @@ import pytest,pytest_check as check
 
 class Test:
     def test_func(self):
-        check.is_true(False,'error 1-1')
-        check.is_true(False,'error 1-2')
+        check.is_true(True,'error 1-1')
+        check.is_true(True,'error 1-2')
 
     def test_func2(self):
-        check.is_true(False,'error 2-1')
+        check.is_true(True,'error 2-1')
         check.is_true(True,'error 2-2')
