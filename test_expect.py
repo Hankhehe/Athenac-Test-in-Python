d5fab6cd09de28750741761f19e6635717f7d29d
unknown <user@Win10Hank.pixis.com>
2021-12-23 16:20:25 +0800

    fix

--- test_expect.py
+++ test_expect.py
@@ -2,9 +2,18 @@ import pytest,pytest_check as check
 
 class Test:
     def test_func(self):
-        check.is_true(True,'error 1-1')
+        check.is_true(False,'error 1-1')
         check.is_true(True,'error 1-2')
 
     def test_func2(self):
+        check.is_true(True,'error 2-1')
+        check.is_true(True,'error 2-2')
+
+class Test2:
+    def test_func3(self):
+        check.is_true(False,'error 1-1')
+        check.is_true(True,'error 1-2')
+
+    def test_func4(self):
         check.is_true(True,'error 2-1')
         check.is_true(True,'error 2-2')
--- testconsole.py
+++ testconsole.py
@@ -12,19 +12,4 @@ import threading
 import hashlib
 
 WebAPI = AthenacWebAPILibry('http://192.168.21.180:8000','admin','admin')
-coreaction = AthenacCoreAPILibry('https://192.168.21.180:18000','7467117531','6922375401')
-# lan2 = PacketAction('Ethernet2')
-time.sleep(5)
-# WebAPI.UpdateBlockMessage(enable=True,ADverify=True,DBverify=True,LDAPverify=True,siteid=1)
-WebAPI.GetMACDetail('005056AEAA69',2)
-coreaction.AuthMACFromUserApply('172.18.255.11','005056AEAA69','RAJ','AgRAu+JjydaLEw3me8kTxA==')
-
-#AD account = Hank pwd = QkIHIDPyeiIALps4IKGH+w==
-#DB account = admin pwd = 36IqJwCHVwl9IS4w4b1mMw==
-#LDAP account = RAJ pwd = AgRAu+JjydaLEw3me8kTxA==
-pass
-# check =   lan2.NDPBlockCheck(srcIP=lan2.linklocalIp,dstIP=lan2.gatewatIpv6,ProbeMAC='00:aa:ff:ae:2b:a1')
-# print(check)
-
-
-# WebAPI.SwitchSiteMonitMode(enable=False,siteid=1)
+print('Stop')
