ef2eda341b9b72f0740ffafb668e612a1b27320e
Hank <hz4154261@gmail.com>
2022-10-25 10:56:55 +0800

    fix

--- SnortAPI/SnortAPI.py
+++ SnortAPI/SnortAPI.py
@@ -1,15 +1,22 @@
-import requests,json
+import requests,json,os
 
-
-Files = {'file': open('SnortAPI/1.pcap', 'rb')}
-Header = {'Content-type': 'application/json'}
-r = requests.post('http://192.168.10.12:8009/api/submit',files=Files,verify=False)
-r = json.loads(r.text)
+pcapfiles = os.listdir('./SnortAPI/Pcap')
+APIPath = 'http://192.168.10.12:8009/api/submit'
 resultdata = {'Data':[]}
-ay = r['analyses'][0]['alerts']
-for i in ay:
-    if i['priority'] == 2:
-        resultdata['Data'].append(i)
-with open('SnortAPI/snortData.json','w') as f:
+
+for pcapfile in pcapfiles:
+    Files = {'file': open(f'SnortAPI/Pcap/{pcapfile}', 'rb')}
+    Header = {'Content-type': 'application/json'}
+    r = requests.post(APIPath,files=Files,verify=False)
+    r = json.loads(r.text)
+    r = r['analyses'][0]['alerts']
+    print(f'Get {pcapfile} finished')
+
+    for i in r:
+        if i['priority'] == 1 :
+            resultdata['Data'].append(i)
+    print(f'filter {pcapfile} data finished')
+
+with open('SnortAPI/ResultData.json','w') as f:
     f.write(json.dumps(resultdata,indent=4,ensure_ascii=False)) 
-pass
+    pass
